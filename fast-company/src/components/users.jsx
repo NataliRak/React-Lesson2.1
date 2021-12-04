fc5c834a0bd5a9f7b5baa34efa84024aab437b33
Natali <natali.rak911@gmail.com>
2021-12-04 14:47:06 +0200

    add renren qualities,rate, and main badge

--- fast-company/src/components/users.jsx
+++ fast-company/src/components/users.jsx
@@ -3,28 +3,35 @@ import api from "../api"
 
 const Users = () => {
 
-    
+    const rate ="/5"
     const formatCount =()=> {
-        return count === 0?"никто не тусуется с тобой сегодня": count + "человек тусуется с тобой сегодня"
+        
+        if (count === 2 || count === 3 || count === 4) return count +" " + "человека тусуется с тобой сегодня"
+        return count === 0?"никто не тусуется с тобой сегодня": count + " " + "человек тусуется с тобой сегодня"
     }
-    
+      
     const getBageClasses =()=> {
         let classes = "badge "
         classes+=count === 0 ?"bg-danger" : "bg-primary"
         return classes
-
     }
+    
      const [users, setUsers] = useState(api.users.fetchAll())
      const [count, setCount] = useState(users.length)
 
      const handleDelete =(id) => {
         setCount((prevState)=>prevState -1)
         setUsers((prevState)=>prevState.filter((user)=>user !== id)) 
-     }
-     
-        
-     
-   
+    }
+
+     const renderBadges = (qualities) => {
+        return qualities.map((quality) => (
+          <span key={quality._id} className={`badge bg-${quality.color} m-1`}>
+            {quality.name}
+          </span>
+        ));
+    };
+    
 
     return (
         <>
@@ -32,8 +39,8 @@ const Users = () => {
                  <span className ={getBageClasses()}>{formatCount()}</span>
             </h2>
            
-
             <table className="table table-dark table-hover">
+
         <thead>
             <tr >
                 <th scope="col">Имя</th>
@@ -48,20 +55,18 @@ const Users = () => {
         <tbody>
             
             {users.map((user)=> 
-                (<tr key={user._id}>
+                (<tr key={user._id} >
                    <td>{user.name}</td> 
-                   <td>{user.qualities.color}</td>
+                   <td> {renderBadges(user.qualities)}</td>
                    <td>{user.profession.name}</td> 
                    <td>{user.completedMeetings}</td> 
-                   <td>{user.rate}</td>
+                   <td>{user.rate}{rate}</td>
                    <td> <button className = "btn btn-danger btn-sm" onClick = {()=>handleDelete(user)}>delete</button></td>    
                 </tr>)
             )}
                 
         </tbody>
         </table>
-
-            
 
         </>
     )
